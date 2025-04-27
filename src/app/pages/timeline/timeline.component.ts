b11471e0b04c9a37e1237b1bd326a1aeb76e915d
OuFFA-8 <ouffaalaa26@gmail.com>
2025-04-27 19:26:48 +0300

    test

--- src/app/pages/timeline/timeline.component.ts
+++ src/app/pages/timeline/timeline.component.ts
@@ -1,14 +1,39 @@
-import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 import { TranslatePipe } from '@ngx-translate/core';
+
+interface Section {
+  id: number;
+  navTitle: string;
+  navNumber: string;
+  contentTitle: string;
+  contentText: string;
+  imageUrl: string;
+}
 
 @Component({
   selector: 'app-timeline',
-  imports: [TranslatePipe],
+  imports: [],
   templateUrl: './timeline.component.html',
   styleUrl: './timeline.component.css'
 })
-export class TimelineComponent  {
+export class TimelineComponent  implements OnInit {
 
+  sections: Section[] = [
+    { id: 1, navTitle: 'التجاري', navNumber: '2013', contentTitle: 'الشركات والتجاري', contentText: 'يقدم LFSH الدعم القانوني...', imageUrl: '/images/black-white-view-theatre.jpg' },
+    { id: 2, navTitle: 'المصرفي', navNumber: '2015', contentTitle: 'الأعمال المصرفية', contentText: 'تفاصيل حول الخدمات...', imageUrl: '/images/black-white-view-theatre.jpg' },
+    { id: 3, navTitle: 'التأمين', navNumber: '2200', contentTitle: 'قانون التأمين', contentText: 'نقدم استشارات شاملة...', imageUrl: '/images/black-white-view-theatre.jpg' },
+    { id: 4, navTitle: 'الاستثمار', navNumber: '2004', contentTitle: 'الاستثمار', contentText: 'دعم قانوني لفرص...', imageUrl: '/images/black-white-view-theatre.jpg' }
+  ];
 
+  selectedSection: Section | null = null;
+  ngOnInit(): void {
+    if (this.sections.length > 0) {
+      this.selectedSection = this.sections[0];
+    }
+  }
+
+  selectSection(section: Section): void {
+    this.selectedSection = section;
+  }
 
 }
