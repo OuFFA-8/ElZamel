d9be5245f6f3e7b6717590f6dd87a30fac181661
OuFFA-8 <ouffaalaa26@gmail.com>
2025-05-17 05:15:01 +0300

    timeline

--- src/app/pages/timeline/timeline.component.ts
+++ src/app/pages/timeline/timeline.component.ts
@@ -10,6 +10,7 @@ interface Section {
   contentText2?: string;
   contentText3?: string;
   contentText4?: string;
+  contentText5?: string;
   imageUrl: string;
 }
 
@@ -25,7 +26,7 @@ export class TimelineComponent implements OnInit {
     { id: 1, navTitle: 'timeline.1.title', contentTitle: 'timeline.1.mainTitle', navNumber: '2013', contentText1: 'timeline.1.paragraph', imageUrl: '/images/pexels-sora-shimazaki-5668484.jpg', contentText2: 'timeline.1.paragraph2', contentText3: 'timeline.1.paragraph3', contentText4: 'timeline.1.paragraph4' },
     { id: 2, navTitle: 'timeline.2.title', contentTitle: 'timeline.2.mainTitle', navNumber: '2015', contentText1: 'timeline.2.paragraph', imageUrl: '/images/pexels-sora-shimazaki-5668484.jpg', },
     { id: 3, navTitle: 'timeline.3.title', contentTitle: 'timeline.3.mainTitle', navNumber: '2200', contentText1: 'timeline.3.paragraph', imageUrl: '/images/pexels-sora-shimazaki-5668484.jpg', },
-    { id: 4, navTitle: 'timeline.4.title', contentTitle: 'timeline.4.mainTitle', navNumber: '2004', contentText1: 'timeline.4.paragraph', imageUrl: '/images/pexels-sora-shimazaki-5668484.jpg', }
+    { id: 4, navTitle: 'timeline.4.title', contentTitle: 'timeline.4.mainTitle', navNumber: '2004', contentText1: 'timeline.4.paragraph', imageUrl: '/images/pexels-sora-shimazaki-5668484.jpg',  contentText2: 'timeline.4.paragraph2', contentText3: 'timeline.4.paragraph3', contentText4: 'timeline.4.paragraph4', contentText5: 'timeline.4.paragraph5'}
   ];
 
   selectedSection: Section | null = null;
