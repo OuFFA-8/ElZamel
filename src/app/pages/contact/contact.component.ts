95d854b6fdc09ac389de4732d161df2d817ba629
OuFFA-8 <ouffaalaa26@gmail.com>
2025-06-30 19:08:11 +0300

    sa

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import { Component, ChangeDetectorRef, OnInit, Renderer2, ElementRef, Inject, PLATFORM_ID } from '@angular/core';
+import { Component, ChangeDetectorRef, OnInit, Renderer2, ElementRef, Inject, PLATFORM_ID, OnDestroy } from '@angular/core';
 import { isPlatformBrowser, CommonModule, DOCUMENT } from '@angular/common'; // استورد isPlatformBrowser و DOCUMENT
 import { trigger, state, style, animate, transition, query } from '@angular/animations';
 import { SafeUrlPipe } from '../../shared/pipes/safe-url.pipe';
@@ -77,19 +77,19 @@ interface SocialLink {
     ])
   ]
 })
-export class ContactComponent implements OnInit {
+export class ContactComponent implements OnInit, OnDestroy {
 
   allBranchesData: BranchDetails[] = [
     { id: 1, name: 'branches.location1', shortName: 'branches.location1', addressLines: 'branches.addressLine1', phoneNumbers: '0114733303', MutualNumber: ' 920009756 ', hours: 'Sat-Thur 8:30am- 5pm', emails: ['zk@zk-legal.com'], mapEmbedUrl: 'https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d4221.407487119673!2d46.719560481823365!3d24.69267349002198!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e2f038f92ad4285%3A0x2c70c9947f5734ac!2sZamil%20and%20Kharashi%20for%20Law%20Firm!5e0!3m2!1sen!2sus!4v1746333306077!5m2!1sen!2sus', socialLinks: [], backgroundImageUrl: '/images/الرياض.webp' },
-    { id: 2, name: 'branches.location2', shortName: 'branches.location2', addressLines: 'branches.addressLine2', phoneNumbers: '0138147677', MutualNumber: ' 920009756', hours: 'Sat-Thur 8:30am- 5pm', emails: [' info@zk-legal.com'], mapEmbedUrl: 'https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7151.15395905835!2d50.199194!3d26.340194!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e49e8b72d39626b%3A0xfac87c3508eeb3f6!2sNSH%20Tower%2C%206389%20King%20Fahd%20Road%2C%20Al%20Rakah%20Al%20Janubiyah%2C%20Al%20Khobar%2034227%2C%20Saudi%20Arabia!5e0!3m2!1sen!2sus!4v1746333187166!5m2!1sen!2sus', socialLinks: [], backgroundImageUrl: '/images/الاوقاف.webp' },
+    { id: 2, name: 'branches.location2', shortName: 'branches.location2', addressLines: 'branches.addressLine2', phoneNumbers: '0138147677', MutualNumber: ' 920009756', hours: 'Sat-Thur 8:30am- 5pm', emails: [' info@zk-legal.com'], mapEmbedUrl: 'https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7151.15395905835!2d50.199194!3d26.340194!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e49e8b72d39626b%3A0xfac87c3508eeb3f6!2sNSH%20Tower%2C%206389%20King%20Fahd%20Road%2C%20Al%20Rakah%20Al%20Janubiyah%2C%20Al%20Khobar%2034227%2C%20Saudi%20Arabia!5e0!3m2!1sen!2sus!4v1746333187166!5m2!1sen!2sus', socialLinks: [], backgroundImageUrl: '/images/الخبر.webp' },
     { id: 3, name: 'branches.location3', shortName: 'branches.location3', addressLines: 'branches.addressLine3', MutualNumber: '920009756', hours: 'Sat-Thur 8:30am- 5pm', emails: ['jed@zk-legal.com'], mapEmbedUrl: 'https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7419.153308684319!2d39.108289!3d21.602442!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x15c3dbabb0e53c41%3A0xf11d981517c55a1f!2sThe%20Headquarters%20Business%20Park!5e0!3m2!1sen!2sus!4v1746332044326!5m2!1sen!2sus', socialLinks: [], backgroundImageUrl: '/images/جدة.webp' },
   ];
 
   selectedBranch: BranchDetails | null = null;
 
   constructor(private cdRef: ChangeDetectorRef, private renderer: Renderer2,
-    @Inject(DOCUMENT) private document: Document, 
-    @Inject(PLATFORM_ID) private platformId: Object 
+    @Inject(DOCUMENT) private document: Document,
+    @Inject(PLATFORM_ID) private platformId: Object
   ) { }
 
   ngOnInit(): void {
@@ -105,15 +105,25 @@ export class ContactComponent implements OnInit {
     if (!isInitialLoad && this.selectedBranch?.id === branch.id) {
       return;
     }
-if (isPlatformBrowser(this.platformId)) {
+    if (isPlatformBrowser(this.platformId)) {
       this.renderer.setStyle(this.document.body, 'backgroundImage', `url('${branch.backgroundImageUrl}')`);
     }
     this.selectedBranch = null;
     this.cdRef.detectChanges();
     setTimeout(() => {
       this.selectedBranch = branch;
-      this.cdRef.detectChanges(); 
+      this.cdRef.detectChanges();
     }, 0);
+    
+  }
+
+  ngOnDestroy(): void {
+    // هذا الكود سيتم تنفيذه عند مغادرة الصفحة
+    // وسيقوم بإزالة الخلفية من الـ body
+    if (isPlatformBrowser(this.platformId)) {
+      // نضع قيمة فارغة لإزالة الـ inline style الخاص بالخلفية
+      this.renderer.setStyle(this.document.body, 'backgroundImage', ''); 
+    }
   }
 
   getRectangleClasses(branch: BranchDetails): string {
